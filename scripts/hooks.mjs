686ab82379eea215480f4fc1af65752235080616
Chris <chrisk123999@gmail.com>
2025-12-10 13:52:43 -0500

    Spaces

--- scripts/hooks.mjs
+++ scripts/hooks.mjs
@@ -1,5 +1,5 @@
 import {midiEvents} from './events/midi.mjs';
-import { constants } from './lib.mjs';
+import {constants} from './lib.mjs';
 export function registerHooks() {
     Hooks.on(constants.workflowHookNames.preTargeting, midiEvents.preTargeting);
     Hooks.on(constants.workflowHookNames.preItemRoll, midiEvents.preItemRoll);
--- scripts/module.mjs
+++ scripts/module.mjs
@@ -3,11 +3,9 @@ import * as lib from './lib.mjs';
 import * as utils from './utils.mjs';
 import {api} from './api.mjs';
 import {test} from './test.mjs';
- 
 Hooks.once('init', () => {
     registerHooks();
 });
- 
 Hooks.once('ready', () => {
     lib.constants.registeredMacros = new lib.Macros.RegisteredMacros();
     lib.constants.registeredMacros.registerFnMacro(test);
