a063ba35cec0240c99cfd04750a75d9f32f3b7b7
Chris <chrisk123999@gmail.com>
2026-04-23 15:31:54 -0400

    Integrations

--- scripts/hooks.mjs
+++ scripts/hooks.mjs
@@ -1,13 +1,13 @@
 import {constants} from './lib/_module.mjs';
 import * as events from './events/_module.mjs';
-import {documentUtils, queryUtils} from './utilities/_module.mjs';
-import * as applications from './applications/_module.mjs';
+import {queryUtils} from './utilities/_module.mjs';
+import dnd5e from './integration/dnd5e.mjs';
 export function registerHooks() {
     // Sheet Rendering
-    Hooks.on(constants.sheetHookNames.getHeaderControlsActiveEffectConfig, appendHeaderControl);
-    Hooks.on(constants.sheetHookNames.getHeaderControlsActorSheetV2, appendHeaderControl);
-    Hooks.on(constants.sheetHookNames.getHeaderControlsItemSheet5e, appendHeaderControl);
-    Hooks.on(constants.sheetHookNames.getHeaderControlsRegionConfig, appendHeaderControl);
+    Hooks.on(constants.sheetHookNames.getHeaderControlsActiveEffectConfig, dnd5e.appendHeaderControl);
+    Hooks.on(constants.sheetHookNames.getHeaderControlsActorSheetV2, dnd5e.appendHeaderControl);
+    Hooks.on(constants.sheetHookNames.getHeaderControlsItemSheet5e, dnd5e.appendHeaderControl);
+    Hooks.on(constants.sheetHookNames.getHeaderControlsRegionConfig, dnd5e.appendHeaderControl);
     // Workflow Events
     Hooks.on(constants.workflowHookNames.preTargeting, events.workflowEvents.preTargeting);
     Hooks.on(constants.workflowHookNames.preItemRoll, events.workflowEvents.preItemRoll);
@@ -53,76 +53,5 @@ export function registerHooks() {
         Hooks.on(constants.itemHookNames.updateItem, events.itemEvents.updateItem);
         // Time Events
         Hooks.on(constants.timeHookNames.updateWorldTime, events.timeEvents.updateWorldTime);
-    }
-}
-
-// TODO: where should we put this
-function appendHeaderControl(app, controls) {
-    if (app.classList.contains('tidy5e-sheet')) return;
-    if (app instanceof foundry.applications.sidebar.apps.Compendium) {
-        const validTypes = ['ActiveEffect', 'Actor', 'Item'];
-        if (!validTypes.includes(app.collection.metadata.type)) return;
-    }
-    const embeddedOnlyTypes = ['Region'];
-    const documentType = app.document?.documentName;
-    const headerLabel = _loc('CAT.MEDKIT.HeaderLabel');
-    if (embeddedOnlyTypes.includes(documentType)) {
-        controls.push({
-            label: headerLabel,
-            icon: 'fa-solid fa-cat',
-            onClick: () => {} // TODO: Embedded Macros
-        });
-        return;
-    }
-    controls.push({
-        label: headerLabel,
-        icon: 'fa-solid fa-cat',
-        onClick: () => {
-            if (app instanceof foundry.applications.sidebar.apps.Compendium) {
-                // TODO: Compendium Medkit
-            } else {
-                // TODO: This properly
-                if (documentType === 'Item') new applications.ItemMedkit({document: app.document}).render({force: true});
-            }
-        }
-    });
-    // TODO: See whether we can color-code some other way
-    if (documentType === 'Item') {
-        setTimeout(async () => {
-            const parentWindow = foundry.applications.detached.windows.get(app.window.windowId)?.window?.document ?? document;
-            const contextItems = parentWindow.querySelectorAll('nav#context-menu .context-item');
-            const headerButton = Array.from(contextItems).find(i => i.innerText.includes(headerLabel))?.querySelector('i');
-            if (!headerButton) return;
-            const item = app.document;
-            if (!item) return;
-            const source = documentUtils.getSource(item);
-            const sources = [
-                'chris-premades',
-                'gambits-premades',
-                'midi-item-showcase-community',
-                'automated-crafted-creations'
-            ];
-            if (!sources.includes(source) && source) {
-                headerButton.dataset.medkitStatus = constants.MEDKIT_STATUSES.UNKNOWN;
-                return;
-            }
-            const statusSuffix = source === 'chris-premades' ? 'CPR' : 'OTHER';
-            let medkitStatus;
-            switch (documentUtils.getAutomationStatus(item)) {
-                case -1:
-                    medkitStatus = constants.MEDKIT_STATUSES.AVAILABLE;
-                    break;
-                case 0:
-                    medkitStatus = constants.MEDKIT_STATUSES[`OUTDATED_${statusSuffix}`];
-                    break;
-                case 1:
-                    medkitStatus = constants.MEDKIT_STATUSES[`UP_TO_DATE_${statusSuffix}`];
-                    break;
-                case 2:
-                case 3:
-                    medkitStatus = constants.MEDKIT_STATUSES.CONFIGURABLE;
-            }
-            if (medkitStatus) headerButton.dataset.medkitStatus = medkitStatus;
-        }, 100);
     }
 }
--- scripts/integration/dnd5e.mjs
+++ scripts/integration/dnd5e.mjs
@@ -1,5 +1,6 @@
 import {default as constants} from '../lib/constants.mjs';
 import documentUtils from '../utilities/documentUtils.mjs';
+import * as applications from '../applications/_module.mjs';
 async function registerAutomations() {
     const packs = [
         'items',
@@ -48,7 +49,77 @@ async function registerScales() {
         }));
     }));
 }
+function appendHeaderControl(app, controls) {
+    if (app.classList.contains('tidy5e-sheet')) return;
+    if (app instanceof foundry.applications.sidebar.apps.Compendium) {
+        const validTypes = ['ActiveEffect', 'Actor', 'Item'];
+        if (!validTypes.includes(app.collection.metadata.type)) return;
+    }
+    const embeddedOnlyTypes = ['Region'];
+    const documentType = app.document?.documentName;
+    const headerLabel = _loc('CAT.MEDKIT.HeaderLabel');
+    if (embeddedOnlyTypes.includes(documentType)) {
+        controls.push({
+            label: headerLabel,
+            icon: 'fa-solid fa-cat',
+            onClick: () => {} // TODO: Embedded Macros
+        });
+        return;
+    }
+    controls.push({
+        label: headerLabel,
+        icon: 'fa-solid fa-cat',
+        onClick: () => {
+            if (app instanceof foundry.applications.sidebar.apps.Compendium) {
+                // TODO: Compendium Medkit
+            } else {
+                // TODO: This properly
+                if (documentType === 'Item') new applications.ItemMedkit({document: app.document}).render({force: true});
+            }
+        }
+    });
+    // TODO: See whether we can color-code some other way
+    if (documentType === 'Item') {
+        setTimeout(async () => {
+            const parentWindow = foundry.applications.detached.windows.get(app.window.windowId)?.window?.document ?? document;
+            const contextItems = parentWindow.querySelectorAll('nav#context-menu .context-item');
+            const headerButton = Array.from(contextItems).find(i => i.innerText.includes(headerLabel))?.querySelector('i');
+            if (!headerButton) return;
+            const item = app.document;
+            if (!item) return;
+            const source = documentUtils.getSource(item);
+            const sources = [
+                'chris-premades',
+                'gambits-premades',
+                'midi-item-showcase-community',
+                'automated-crafted-creations'
+            ];
+            if (!sources.includes(source) && source) {
+                headerButton.dataset.medkitStatus = constants.MEDKIT_STATUSES.UNKNOWN;
+                return;
+            }
+            const statusSuffix = source === 'chris-premades' ? 'CPR' : 'OTHER';
+            let medkitStatus;
+            switch (documentUtils.getAutomationStatus(item)) {
+                case -1:
+                    medkitStatus = constants.MEDKIT_STATUSES.AVAILABLE;
+                    break;
+                case 0:
+                    medkitStatus = constants.MEDKIT_STATUSES[`OUTDATED_${statusSuffix}`];
+                    break;
+                case 1:
+                    medkitStatus = constants.MEDKIT_STATUSES[`UP_TO_DATE_${statusSuffix}`];
+                    break;
+                case 2:
+                case 3:
+                    medkitStatus = constants.MEDKIT_STATUSES.CONFIGURABLE;
+            }
+            if (medkitStatus) headerButton.dataset.medkitStatus = medkitStatus;
+        }, 100);
+    }
+}
 export default {
     registerAutomations,
-    registerScales
+    registerScales,
+    appendHeaderControl
 };
