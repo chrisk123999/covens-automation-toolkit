0450edbae2d736cb15b37b50a65fbe7c47894bc7
Chris <chrisk123999@gmail.com>
2026-06-02 12:22:44 -0400

    More Utils

--- scripts/utilities/animationUtils.mjs
+++ scripts/utilities/animationUtils.mjs
@@ -5,7 +5,33 @@ function getAnimation(source, identifier) {
 async function preloadAnimations(animations, {showProgressBar} = {}) {
     return await Sequencer.Preloader.preloadForClients(animations, showProgressBar);
 }
+function simpleAttack(sourceToken, targetToken, animation, {sound, missed = false} = {}) {
+    /* eslint-disable indent */
+    new Sequence()
+        .effect()
+            .atLocation(sourceToken)
+            .stretchTo(targetToken)
+            .file(animation)
+            .missed(missed)
+        .sound()
+            .playIf(sound)
+            .file(sound)
+        .play();
+    /* eslint-enable indent */
+}
+function buildColorOptions(colorMap, {freeColors = [], labelPrefix = '', random, cycle, requirements = []} = {}) {
+    const options = {};
+    for (const [key, label] of Object.entries(colorMap)) {
+        options[key] = {label: labelPrefix + label};
+        if (!freeColors.includes(key)) options[key].requirements = requirements;
+    }
+    if (random) options.random = {label: labelPrefix + 'Random', requirements};
+    if (cycle) options.cycle = {label: labelPrefix + 'Cycle', requirements};
+    return options;
+}
 export default {
     getAnimation,
-    preloadAnimations
+    preloadAnimations,
+    simpleAttack,
+    buildColorOptions
 };
--- scripts/utilities/workflowUtils.mjs
+++ scripts/utilities/workflowUtils.mjs
@@ -84,13 +84,73 @@ async function syntheticActivityDataRoll(activityData, item, targets, {config = 
     const activity = newItem.system.activities.get(activityData._id);
     return await syntheticActivityRoll(activity, targets, {config, options, dialog, message, userId, atLevel, consumeUsage, consumeResources, spellSlot});
 }
-async function completeItemUse(item, targets = []) {
-
+async function completeItemUse(item, targets = [], {config = {}, options = {}, dialog = {}, message = {}, userId, atLevel, consumeUsage = true, consumeResources = true, spellSlot = true, fast = false, autoDamage} = {}) {
+    const defaultConfig = {
+        consumeUsage,
+        consume: {
+            resources: consumeResources,
+            spellSlot
+        }
+    };
+    const defaultOptions = {
+        targetUuids: targets.map(i => i.uuid),
+        configureDialog: false,
+        workflowOptions: {
+            autoFastDamage: fast,
+            autoRollAttack: fast
+        }
+    };
+    if (autoDamage) {
+        let autoRollDamage = MidiQOL.configSettings().autoRollDamage;
+        if (!['always', 'onHit'].includes(autoRollDamage)) autoRollDamage = 'onHit';
+        defaultOptions.workflowOptions.autoRollDamage = autoRollDamage;
+    }
+    if (atLevel) {
+        const spellLabel = actorUtils.getEquivalentSpellSlotName(item.actor, atLevel);
+        if (spellLabel) defaultConfig.spell = {slot: spellLabel};
+    }
+    if (userId) {
+        options.asUser ||= userId;
+    } else {
+        options.asUser ||= queryUtils.firstOwner(item.actor, true);
+    }
+    options = genericUtils.mergeObject(defaultOptions, options);
+    config = genericUtils.mergeObject(defaultConfig, config);
+    config.midiOptions = options;
+    let fixSets = false;
+    if (!config.midiOptions?.asUser && !queryUtils.hasPermission(item.actor, game.userId)) {
+        if (!config.midiOptions) config.midiOptions = {};
+        config.midiOptions.asUser = queryUtils.firstOwner(item.actor, true);
+        config.midiOptions.checkGMStatus = true;
+        config.midiOptions.workflowData = true;
+        fixSets = true;
+    } else if (config.midiOptions?.asUser && config.midiOptions?.asUser !== game.userId) {
+        config.midiOptions.workflowData = true;
+        fixSets = true;
+    }
+    let workflow = await MidiQOL.completeItemUse(item, config, dialog, message);
+    workflow = workflow.workflow ?? workflow;
+    if (fixSets) {
+        if (workflow.failedSaves) workflow.failedSaves = new Set(workflow.failedSaves);
+        if (workflow.hitTargets) workflow.hitTargets = new Set(workflow.hitTargets);
+        if (workflow.targets) workflow.targets = new Set(workflow.targets);
+    }
+    return workflow;
+}
+async function syntheticItemRoll(item, targets = [], {config = {}, options = {}, dialog = {}, message = {}, userId, atLevel, consumeUsage = true, consumeResources = true, spellSlot = true} = {}) {
+    return await completeItemUse(item, targets, {config, options, dialog, message, userId, atLevel, consumeUsage, consumeResources, spellSlot, fast: true, autoDamage: true});
+}
+async function syntheticItemDataRoll(itemData, actor, targets = [], {config = {}, options = {}, dialog = {}, message = {}, userId, atLevel, consumeUsage = true, consumeResources = true, spellSlot = true} = {}) {
+    const newItem = itemUtils.syntheticItem(itemData, actor);
+    return await syntheticItemRoll(newItem, targets, {config, options, dialog, message, userId, atLevel, consumeUsage, consumeResources, spellSlot});
 }
 export default {
     getActionType,
     isAttackType,
     completeActivityUse,
     syntheticActivityRoll,
-    syntheticActivityDataRoll
+    syntheticActivityDataRoll,
+    completeItemUse,
+    syntheticItemRoll,
+    syntheticItemDataRoll
 };
