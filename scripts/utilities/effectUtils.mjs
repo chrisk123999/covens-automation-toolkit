97427af8435c52f4559d899f3feb60bfd7764ecc
bacon-nugget <122384608+bacon-nugget@users.noreply.github.com>
2026-07-12 18:08:18 -0400

    use getter in getOriginActivity

--- scripts/utilities/effectUtils.mjs
+++ scripts/utilities/effectUtils.mjs
@@ -53,7 +53,7 @@ async function getOriginActivity(effect) {
     if (originActivityUuid) return await fromUuid(originActivityUuid);
     if (origin.parent?.documentName === 'Item') {
         return origin.parent.system.activities?.find(activity => 
-            activity.effects.some(aEffect => aEffect._id === origin.id)
+            activity.effects.some(aEffect => aEffect.effect.id === origin.id)
         );
     }
 }
@@ -67,7 +67,7 @@ function getOriginActivitySync(effect) {
     if (originActivityUuid) return fromUuidSync(originActivityUuid, {strict: false});
     if (origin.parent?.documentName === 'Item') {
         return origin.parent.system.activities?.find(activity => 
-            activity.effects.some(aEffect => aEffect._id === origin.id)
+            activity.effects.some(aEffect => aEffect.effect.id === origin.id)
         );
     }
 }
