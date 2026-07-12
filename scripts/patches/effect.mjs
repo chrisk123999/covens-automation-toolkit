d967543ebfc24cee5c93510379b8aedbcf958ff9
bacon-nugget <122384608+bacon-nugget@users.noreply.github.com>
2026-07-11 23:56:05 -0400

    fix doCreate event

--- scripts/patches/effect.mjs
+++ scripts/patches/effect.mjs
@@ -1,18 +1,24 @@
 import {effectEvents} from '../events/_module.mjs';
 import {Logging} from '../lib/_module.mjs';
-async function create(wrapped, data = {}, operation = {}) {
-    let result = await effectEvents.doCreateActiveEffect(data, operation);
-    if (result) return;
-    let effect =  await wrapped(data, operation);
-    return effect;
+async function create(wrapped, data = [], operation = {}) {
+    const filtered = [];
+    for (let i = 0; i < data.length; i++) {
+        const effectData = data[i];
+        if (await effectEvents.doCreateActiveEffect(effectData, operation)) {
+            Logging.addEntry('DEBUG', `Effect creation prevented by doCreate event | Parent UUID: ${operation.parent?.uuid ?? operation.parentUuid} | Effect: ${i + 1} of ${data.length}`);
+            continue;
+        }
+        filtered.push(effectData);
+    }
+    return await wrapped(filtered, operation);
 }
 function patch(enabled) {
     if (enabled) {
-        Logging.addEntry('DEBUG', 'Patching: ActiveEffect.implementation.create');
-        libWrapper.register('cat', 'ActiveEffect.implementation.create', create, 'MIXED');
+        Logging.addEntry('DEBUG', 'Patching: ActiveEffect.implementation.createDocuments');
+        libWrapper.register('cat', 'ActiveEffect.implementation.createDocuments', create, 'MIXED');
     } else {
-        Logging.addEntry('DEBUG', 'Unpatching: ActiveEffect.implementation.create');
-        libWrapper.unregister('cat', 'ActiveEffect.implementation.create');
+        Logging.addEntry('DEBUG', 'Unpatching: ActiveEffect.implementation.createDocuments');
+        libWrapper.unregister('cat', 'ActiveEffect.implementation.createDocuments');
     }
 }
 export default {
