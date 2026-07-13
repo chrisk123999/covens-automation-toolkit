7e42dd5ee01cf0dd3a2e7f79430a1fdccf2da03b
Chris <chrisk123999@gmail.com>
2026-07-13 11:01:30 -0400

    await this

--- scripts/mechanics/smite.mjs
+++ scripts/mechanics/smite.mjs
@@ -3,7 +3,7 @@ import {actorUtils, workflowUtils} from '../utilities/_module.mjs';
 export async function smite(workflow) {
     if (!workflowUtils.isAttackType(workflow, 'attack')) return;
     if (actorUtils.hasUsedBonusAction(workflow.actor)) return;
-    const smites = new Events.WorkflowEvent(constants.workflowPasses.smite, workflow).run({multiResult: true, canOverlap: true});
+    const smites = await new Events.WorkflowEvent(constants.workflowPasses.smite, workflow).run({multiResult: true, canOverlap: true});
     if (!smites.length) return;
-
+    
 }
